b277e64542411f6c98ba83a03e1737d114dc8ab2
mgallet <mgallet@spibol.19pouces.net>
2015-08-24 00:10:53 +0200

    bug fix in URL

--- penatesserver/root_urls.py
+++ penatesserver/root_urls.py
@@ -13,7 +13,7 @@ urls = [
     url(r'^', include(router.urls)),
     url(r'^no-auth/get_host_keytab/(?P<hostname>[a-zA-Z0-9\.\-_]+)$', 'penatesserver.views.get_host_keytab'),
     url(r'^auth/get_info/$', 'penatesserver.views.get_info'),
-    url(r'^auth/set_dhcp/(?P<mac_address>^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2}))$', 'penatesserver.views.set_dhcp'),
+    url(r'^auth/set_dhcp/(?P<mac_address>([0-9A-Fa-f]{2}[:-]){5}[0-9A-Fa-f]{2})/$', 'penatesserver.views.set_dhcp'),
     url(r'^auth/conf/dhcpd.conf$', 'penatesserver.views.get_dhcpd_conf'),
     url(r'^auth/get_host_certificate/$', 'penatesserver.views.get_host_certificate'),
     url(r'^auth/get_ssh_pub/$', 'penatesserver.views.get_ssh_pub'),
