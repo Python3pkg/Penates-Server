98f8cd58f1f9bb57dc5f611642a5b3425ad34cc1
mgallet <mgallet@spibol.19pouces.net>
2015-08-29 17:33:25 +0200

    better kerberos options

--- penatesserver/kerb.py
+++ penatesserver/kerb.py
@@ -53,8 +53,7 @@ def add_principal(principal):
     if principal_exists(principal):
         return
     if settings.KERBEROS_IMPL == 'mit':
-        if Principal.objects.filter(name=principal).count() == 0:
-            Principal(name=principal).save()
+        Principal(name=principal).save()
     else:
         heimdal_command('add', '--random-key', '--max-ticket-life=1d', '--max-renewable-life=1w', '--attributes=',
                         '--expiration-time=never', '--pw-expiration-time=never', '--policy=default', principal)
