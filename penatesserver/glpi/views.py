863a347345b47bb6e0f4a0eed73f47a47a2ab758
mgallet <mgallet@spibol.19pouces.net>
2015-12-20 12:54:22 +0100

    Better register Shinken checks

--- penatesserver/glpi/views.py
+++ penatesserver/glpi/views.py
@@ -63,7 +63,7 @@ def register_service(request, check_command):
         form = ShinkenServiceForm(request.GET)
         if not form.is_valid():
             return HttpResponse(status=400)
-        values = {key: value for key, value in form.cleaned_data.items() if value is not None}
+        values = {key: value for key, value in form.cleaned_data.items() if value}
         if ShinkenService.objects.filter(host_name=fqdn, check_command=check_command)\
                 .update(**values) == 0:
             ShinkenService(host_name=fqdn, check_command=check_command, **values).save()
